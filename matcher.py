3aabe372adfac5b65567121989af4e727182bbdb
Kunal <kunal271202@gmail.com>
2026-04-26 13:55:24 +0530

    Create matcher.py

---
+++ matcher.py
@@ -0,0 +1,10 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+def match_with_jd(job_desc, candidates):
+    # Combine all candidate skills into strings
+    skills_list = [' '.join(c['skills']) for c in candidates]
+    vectorizer = TfidfVectorizer()
+    tfidf_matrix = vectorizer.fit_transform([job_desc] + skills_list)
+    similarities = cosine_similarity(tfidf_matrix[0], tfidf_matrix[1:])
+    return similarities.flatten()
